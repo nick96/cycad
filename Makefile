39cb527b007a38764d1561e80410995e667ca23c
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-18 15:28:55 +1000

    Cleanup editorservice gateway

--- Makefile
+++ Makefile
@@ -4,13 +4,18 @@ PROTOC_ARGS=-I./third_party/googleapis
 
 EDITOR_PROTO=editorservice/service.proto
 
-editor-stub:
+all: editor
+
+editor-pb:
 	$(PROTOC) $(PROTOC_ARGS) -Ieditorservice --go_out=plugins=grpc,paths=source_relative:./editorservice/pb $(EDITOR_PROTO)
 
-editor-gateway:
+editor-gateway-pb:
 	$(PROTOC) $(PROTOC_ARGS) -Ieditorservice --grpc-gateway_out=logtostderr=true,paths=source_relative:./editorservice/pb $(EDITOR_PROTO)
 
 editor-swagger:
 	$(PROTOC) $(PROTOC_ARGS) -Ieditorservice --swagger_out=logtostderr=true:./editorservice/swagger $(EDITOR_PROTO)
 
-editor: editor-stub editor-gateway editor-swagger
+editor-gateway: editor-gateway-pb editor-pb
+	cd editorservice && go build -o gateway/gateway github.com/nick96/cycad/editorservice/gateway
+
+editor: editor-swagger editor-gateway
--- editorservice/gateway/main.go
+++ editorservice/gateway/main.go
@@ -1,46 +1,48 @@
 package main
 
 import (
-  "context"
-  "flag"
-  "net/http"
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
 
-  "github.com/golang/glog"
-  "github.com/grpc-ecosystem/grpc-gateway/runtime"
-  "google.golang.org/grpc"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
 
-  gw "github.com/nick96/cycad/editorservice/editorservicepb"
+	gw "github.com/nick96/cycad/editorservice/pb"
 )
 
 var (
-  // command-line options:
-  // gRPC server endpoint
-  grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:9090", "gRPC server endpoint")
+	endpoint = flag.String("endpoint", "localhost:9090", "gRPC server endpoint")
+	port     = flag.String("port", "8081", "Port to listen on")
 )
 
 func run() error {
-  ctx := context.Background()
-  ctx, cancel := context.WithCancel(ctx)
-  defer cancel()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-  // Register gRPC server endpoint
-  // Note: Make sure the gRPC server is running properly and accessible
-  mux := runtime.NewServeMux()
-  opts := []grpc.DialOption{grpc.WithInsecure()}
-  err := gw.RegisterYourServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
-  if err != nil {
-    return err
-  }
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterEditorServiceHandlerFromEndpoint(ctx, mux, *endpoint, opts)
+	if err != nil {
+		return err
+	}
+	glog.Info("Registered editor service at %s", *endpoint)
 
-  // Start HTTP server (and proxy calls to gRPC server endpoint)
-  return http.ListenAndServe(":8081", mux)
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	glog.Info("Listening on port %s", *port)
+	return http.ListenAndServe(fmt.Sprintf(":%s", *port), mux)
 }
 
 func main() {
-  flag.Parse()
-  defer glog.Flush()
+	flag.Parse()
+	defer glog.Flush()
 
-  if err := run(); err != nil {
-    glog.Fatal(err)
-  }
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
 }
