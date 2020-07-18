695ffbf99fd83774c57bb9c79298b0091dc50726
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-18 17:48:38 +1000

    Enabled the gRPC reflection

--- editorservice/main.go
+++ editorservice/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/golang/glog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	pb "github.com/nick96/cycad/editorservice/pb"
 )
@@ -24,11 +25,13 @@ type editorServiceServer struct {
 }
 
 func (s *editorServiceServer) AddNode(ctx context.Context, req *pb.AddNodeRequest) (*pb.AddNodeResponse, error) {
-	return nil, errNotImplemented
+	glog.Infof("Received AddNode request: %v", req)
+	return &pb.AddNodeResponse{Name: "name", Content: "content"}, nil
 }
 
 func (s *editorServiceServer) GetNode(ctx context.Context, req *pb.GetNodeRequest) (*pb.GetNodeResponse, error) {
-	return nil, errNotImplemented
+	glog.Infof("Received GetNode request: %v", req)
+	return &pb.GetNodeResponse{Name: "name", Content: "content"}, nil
 }
 
 func newServer() *editorServiceServer {
@@ -40,12 +43,20 @@ func main() {
 	flag.Parse()
 	defer glog.Flush()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
+	// This needs to be 0.0.0.0 so that it works in a docker container. I
+	// used localhost before and it was very frustrating to debug! It'll
+	// work locally but the connection is refused if you call it from
+	// outside the container.
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", *port))
 	if err != nil {
-		glog.Exit("failed to listen on port %s: %v", *port, err)
+		glog.Exitf("Failed to listen on port %s: %v", *port, err)
 	}
 
 	svr := grpc.NewServer()
 	pb.RegisterEditorServiceServer(svr, newServer())
-	svr.Serve(lis)
+	reflection.Register(svr)
+	glog.Infof("Starting editor service on port %s", *port)
+	if err := svr.Serve(lis); err != nil {
+		glog.Exitf("Server exited: %v", err)
+	}
 }
