91d037ea4c851c7d55108994d4704807d3c7068b
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-18 17:48:16 +1000

    Check the conection the the editorservice endpoint on gateway startup

--- editorservice/gateway/main.go
+++ editorservice/gateway/main.go
@@ -23,18 +23,21 @@ func run() error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	// Register gRPC server endpoint
-	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := gw.RegisterEditorServiceHandlerFromEndpoint(ctx, mux, *endpoint, opts)
-	if err != nil {
+	if conn, err := grpc.Dial(*endpoint, opts...); err != nil {
+		glog.Exitf("Failed to connect to endpoint %s: %v", *endpoint, err)
+	} else {
+		glog.Infof("Successfully dialed endpoint %s", *endpoint)
+		conn.Close()
+	}
+	if err := gw.RegisterEditorServiceHandlerFromEndpoint(ctx, mux, *endpoint, opts); err != nil {
 		return err
 	}
-	glog.Info("Registered editor service at %s", *endpoint)
+	glog.Infof("Registered editor service at %s", *endpoint)
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	glog.Info("Listening on port %s", *port)
+	glog.Infof("Listening on port %s", *port)
 	return http.ListenAndServe(fmt.Sprintf(":%s", *port), mux)
 }
 
